4e9e06c187d85e17069d19e86abf2556b8dbaaf4
mike Jones <mikiej_boy@hotmail.com>
2016-02-18 19:22:10 +0000

    Update statemachine. One initial state of patrol.
    Statemachine is attached to the unit, this upon start up creates new patrol state.
    
    Patrol state handles the patrolling of the unit. upon being within a certain distance of the player - the state should call an update to the statemanager with a new chase state added.
    The chase state should handle the attack state - that is upon getting within a  closer range shots can be fired.
    
    The attack state will then either revert back to chase state or return to patrol state upon loosing or destroying plyer unit

---
+++ Tanks/Assets/Scripts/Tank/StateManagement/StateManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateManager : MonoBehaviour {
+
+    //protected StateManager manager;
+    protected State currentState;
+    public bool isActive = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("Starting StateManager");
+        currentState = new PatrolState(this);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (currentState != null)
+        {
+            currentState.updateState();
+        }
+	}
+
+    public void switchState(State newState)
+    {
+        currentState = newState;
+    }
+
+    public Transform getTransform()
+    {
+        return transform;
+    }
+
+}
